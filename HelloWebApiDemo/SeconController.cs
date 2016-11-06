8859df45f9a3264f53e9581016cba0aba0f4ae1c
rayabharapusuman <rayabharapusuman@gmail.com>
2016-11-06 13:43:13 -0500

    Understanding Git
    How to upload sample project

---
+++ HelloWebApiDemo/SeconController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace HelloWebApiDemo
+{
+    public class SeconController : ApiController
+    {
+        public string Get()
+        {
+            return "This is second controller";
+        }
+    }
+}
